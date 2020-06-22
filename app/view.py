c8dd6d91ff781c58a897746a9ad0be052aabf47a
DANRAMAGE <dan@inlet.geol.sc.edu>
2020-06-22 13:02:56 -0400

    Added JSON error in get handler.

--- app/view.py
+++ app/view.py
@@ -1394,7 +1394,7 @@ class SitesDataAPI(MethodView):
 
     except Exception as e:
       current_app.logger.exception(e)
-
+      client_results = build_json_error(501, 'Server encountered a problem with the query.')
 
     current_app.logger.debug('IP: %s SiteDataAPI get for site: %s finished in %f seconds' % (request.remote_addr,
                                                                                              sitename,
